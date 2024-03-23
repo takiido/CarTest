6b0edb508b97c0dbf3ad2a84de5434a7372fa4d9
takiido <157808651+takiido@users.noreply.github.com>
2024-03-23 13:23:01 -0500

    created steering vars

--- Assets/CNGN/Core/CNGN_Controller.cs
+++ Assets/CNGN/Core/CNGN_Controller.cs
@@ -4,4 +4,39 @@ using UnityEngine;
 
 public class CNGN_Controller : MonoBehaviour
 {
+    public CNGN_Wheel[] frontWheels;
+    
+    [Header("Car specs")] 
+    public float wheelBase;
+    public float rearTrack;
+    public float turnRadius;
+    
+    [Header("Input")]
+    public float steerInput;
+
+    private float _ackAngleL;
+    private float _ackAngleR;
+
+    private void Update()
+    {
+        steerInput = Input.GetAxis("Horizontal");
+        
+        if (steerInput > 0) // Turning right
+        {
+            
+        }
+        else if (steerInput < 0) // Turning left
+        {
+
+        }
+        else 
+        {
+            
+        }
+    }
+
+    public void OnSteer()
+    {
+        
+    }
 }
