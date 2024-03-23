88937dcfb3dc94a0a4d8f793936911ba0eb499dc
takiido <157808651+takiido@users.noreply.github.com>
2024-03-22 23:59:03 -0500

    scene setup

--- Assets/CNGN/Core/CNGN_Main.cs
+++ Assets/CNGN/Core/CNGN_Main.cs
@@ -9,31 +9,11 @@ public class CNGN_Main : MonoBehaviour
     public GameObject frontRight;
     public GameObject rearLeft;
     public GameObject rearRight;
-    private GameObject[] _wheels;
 
     public float hp;
     private double _speed;
 
     private void Start()
     {
-        AddWheels();
-    }
-
-    private void Accelerate()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            foreach (GameObject wheel in _wheels)
-            {
-            }
-        }
-    }
-
-    private void AddWheels()
-    {
-        _wheels[0] = frontLeft;
-        _wheels[1] = frontRight;
-        _wheels[2] = rearLeft;
-        _wheels[3] = rearRight;
     }
 }
